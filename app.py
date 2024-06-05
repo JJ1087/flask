20fb0edc896cf71dfed762121789be2fb44d4e14
JJ1087 <20200708@uthh.edu.mx>
2024-06-05 17:49:07 -0600

    agregar formulario

--- app.py
+++ app.py
@@ -9,9 +9,25 @@ def home():
             <body>
                 <h1>Jeziel Gonzalez Antonio 9no A</h1>
                 <img src="/static/saludo.webp" alt="Saludos">
+                <form id="greetForm" method="post">
+                    <label for="first_name">¿Cual es tu nombre?</label>
+                    <input type="text" id="first_name" name="first_name"><br>
+                    <label for="last_name">¿Cual es tu apellido?</label>
+                    <input type="text" id="last_name" name="last_name"><br><br>
+                    <input type="button" value="Enviar" onclick="submitForm()">
+                </form>
+                <label id="greetingLabel"></label>
             </body>
+            <script>
+                function submitForm() {
+                    var firstName = document.getElementById('first_name').value;
+                    var lastName = document.getElementById('last_name').value;
+                    document.getElementById('greetingLabel').innerText = `Hola ${firstName} ${lastName}!`;
+                }
+            </script>
         </html>"
     '''
+
 
 if __name__ == '__main__':
     app.run(debug=True)
